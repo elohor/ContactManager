07169261fc0de558e53346e742c7292b4ee5bca7
elohor <elohorthomas@yahoo.com>
2017-09-18 15:18:51 +0000

    added a new contact

--- contactmanager.py
+++ contactmanager.py
@@ -8,7 +8,20 @@ class Contact:
 		self.email_address = email_address
 		self.postal_address = postal_address
 
-newContact = Contact(elohor,+2348023423474, female, elohor.thomas@meltwater.org, 1234)
+	def get_contact(self):
+		print ("Name: {}\n Number: {}\n Gender: {}\n Email: {}\n Postal: {}\n".format(
+			self.name, self.phone_number, self.gender,
+			self.email_address, self.postal_address)
+		)
 
+name = input("Enter name:")
+phone_number = input("Enter phone number:")
+gender = input("Enter gender:")
+email_address = input("Enter email:")
+postal_address = input("Enter Postal Address:")
+
+newContact = Contact(name, phone_number, gender, email_address, postal_address)
+
+newContact.get_contact()
 
 
