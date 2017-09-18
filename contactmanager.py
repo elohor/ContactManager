ae0cc8dfeaf681abe461b877180a3a4c4a10b71b
elohor <elohorthomas@yahoo.com>
2017-09-18 14:48:04 +0000

    added contactmanager

---
+++ contactmanager.py
@@ -0,0 +1,14 @@
+class Contact:
+
+
+	def __init__(self, name, phone_number, gender, email_address, postal_address):
+		self.name = name
+		self.phone_number = phone_number
+		self.gender = gender
+		self.email_address = email_address
+		self.postal_address = postal_address
+
+newContact = Contact(elohor,+2348023423474, female, elohor.thomas@meltwater.org, 1234)
+
+
+
