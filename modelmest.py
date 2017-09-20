6d1cc391c04e6d13d1e095d5ad65e3954e2dd2b8
elohor <elohorthomas@yahoo.com>
2017-09-20 17:01:11 +0000

    added Inheritance feature

--- modelmest.py
+++ modelmest.py
@@ -3,28 +3,31 @@ class School:
 		self.eits = eits
 		self.fellows = fellows
 
-	def add_eit(self, eit):
-		self.eits.append(fellow)
+	# def add_eit(self, eit):
+	# 	self.eits.append(fellow)
 
-	def add_fellow(self, fellow):
-		self.fellows.append(fellow)
+	# def add_fellow(self, fellow):
+	# 	self.fellows.append(fellow)
 
-
-class Eit:
+class Person:
 	def __init__(self, names, nationality):
 		self.names = names
 		self.nationality = nationality
+
+
+class Eit(Person):
+	def __init__(self, names, nationality):
+		super().__init__(names, nationality)
 
 	def fun_facts(self, string = ""):
 		print (string)
 
 
-class Fellow:
+class Fellow(Person):
 	def __init__(self, names, nationality, happiness_level):
-		self.names = names
-		self.nationality = nationality
+		super().__init__(names, nationality)
 		self.happiness_level = happiness_level
-
+		
 	def eat(self, food, happiness_level = 1):
 		self.happiness_level += happiness_level 
 		print ("I ate {} today and my happiness level increased to {}".format(food,self.happiness_level))
@@ -35,13 +38,12 @@ class Fellow:
 
 
 if __name__ == "__main__":
+	Andrew = Fellow("Andrew", "American", 5)
+	Francis = Fellow("Francis", "Ghanian", 5)
+	Elohor = Eit("Elohor", "Nigerian")
+	Simon = Eit("Simon", "Ghanian")
+	Kelvin = Eit("Kelvin", "Ghanian")
+	Andrew.eat("banku", 7)
 
-Andrew = Fellow("Andrew", "American", 5)
-Francis = Fellow("Francis", "Ghanian", 5)
-
-Elohor = Eit("Elohor", "Nigerian")
-Simon = Eit("Simon", "Ghanian")
-Kelvin = Eit("Kelvin", "Ghanian")
-
-add_eit(School(Andrew, Elohor))
-add_fellow(School(Francis, kelvin))
+	# add_eit(School(Andrew, Elohor))
+	# add_fellow(School(Francis, kelvin))
